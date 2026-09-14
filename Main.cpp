c0bf4db828565e45c72cc6dc3bfd03e86f119b00
thsu5957 <thsu5957@naver.com>
2026-09-14 18:16:34 +0900

    커밋

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include "Main_Menu.h"
+#include <Windows.h>
+#include "resource.h"
+
+
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow
+)
+{
+
+   
+}
